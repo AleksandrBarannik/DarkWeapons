1a329b429a6d604fb79ddb2dc0f6bd9670bd17d0
AleksandrBarannik <aleksandrbarannikwork@gmail.com>
2023-03-25 14:44:19 +0300

    HideWall
    Сделано плавное  сокрытие стены

--- Assets/Scripts/Levels/WallVisibility.cs
+++ Assets/Scripts/Levels/WallVisibility.cs
@@ -9,7 +9,10 @@ public class WallVisibility : MonoBehaviour
    private List<MeshRenderer>  _wallMesh;
 
    private float _showTime;
+   
+   private  float altha = 1f;
 
+   
    private void Update()
    {
       if (Time.time > _showTime)
@@ -18,26 +21,29 @@ public class WallVisibility : MonoBehaviour
 
    public void HideFor(float seconds)
    {
-      Hide();
+      StartCoroutine(nameof(Hide));
       _showTime = Time.time + seconds;
-   }
-
-   private void Hide()
-   {
-      foreach (var wallMesh in _wallMesh)
-      {
-         wallMesh.material.color = new Color(1,1,1,0.5f);
-      }
-      
    }
    
    private void Show()
    {
       foreach (var wallMesh in _wallMesh)
-      {
-         wallMesh.material.color = new Color(1,1,1,1);
+      {         
+         wallMesh.material.color = new Color(1,1,1,altha);
+         altha = 1f;
       }
-      
+   }   
+
+   IEnumerator Hide()
+   {
+      while (altha > 0.5f)
+      {
+         foreach (var wallMesh in _wallMesh)
+         {
+            wallMesh.material.color = new Color(1, 1, 1, altha);
+            altha -= Time.deltaTime;
+            yield return altha;
+         }
+      }
    }
-   
 }
